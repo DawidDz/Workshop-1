6e7df3200a3950e2396828977eaf265be9a8d22a
DawidDz <polkycwerty@o2.pl>
2022-04-29 11:44:44 +0200

    wczytanie pliku i wyświetlenie zawartości na konsoli

--- src/pl/coderslab/TaskManager.java
+++ src/pl/coderslab/TaskManager.java
@@ -1,9 +1,27 @@
 package pl.coderslab;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class TaskManager {
 
-    public static void main(String[] args) {
+    public static void readFromFile(String fileName) {
 
+        File file = new File("/home/dawid/Pobrane/Zasoby_do_projektu/05_attachment_Zasoby do projektu.pl/tasks.csv");
+        try (Scanner scan = new Scanner(file)) {
 
+            while (scan.hasNextLine()) {
+                System.out.println(scan.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main (String[]args) {
+
+        readFromFile("tasks.csv");
     }
 }
