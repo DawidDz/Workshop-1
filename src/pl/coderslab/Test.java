a2486b79457f62ef8534733b5681d6a39064c7d7
DawidDz <polkycwerty@o2.pl>
2022-04-29 15:57:46 +0200

    wyświetlanie list i exit

---
+++ src/pl/coderslab/Test.java
@@ -0,0 +1,80 @@
+package pl.coderslab;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+public class Test {
+
+    static final String[] OPTIONS = {"add", "remove", "list", "exit", "-------------"};
+
+    public static void printTab(String[][] tab) {
+        for (int i = 0; i < tab.length; i++) {
+            System.out.print(i + " : ");
+            for (int j = 0; j < tab[i].length; j++) {
+                System.out.print(tab[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void printOptions(String[] tab) {
+        System.out.println(ConsoleColors.BLUE);
+        System.out.println("Please select an option: " + ConsoleColors.RESET);
+        for (String option : tab) {
+            System.out.println(option);
+        }
+    }
+
+    public static String[][] loadDataToTab(String fileName) {
+        Path dir = Paths.get(fileName);
+        if (!Files.exists(dir)) {
+            System.out.println("File not exist.");
+            System.exit(0);
+        }
+
+        String[][] tab = null;
+        try {
+            List<String> strings = Files.readAllLines(dir);
+            tab = new String[strings.size()][strings.get(0).split(",").length];
+
+            for (int i = 0; i < strings.size(); i++) {
+                String[] split = strings.get(i).split(",");
+                System.arraycopy(split, 0, tab[i], 0, split.length);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return tab;
+    }
+
+    public static void main(String[] args) {
+
+        String[][] tasks;
+        printOptions(OPTIONS);
+        tasks = loadDataToTab("/home/dawid/Pobrane/Zasoby_do_projektu/05_attachment_Zasoby do projektu.pl/tasks.csv");
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            switch (input) {
+                case "exit":
+                    System.out.print(ConsoleColors.RED);
+                    System.out.println("Bye, bye.");
+                    break;
+                case "add":
+                    break;
+                case "remove":
+                    break;
+                case "list":
+                    System.out.println(ConsoleColors.PURPLE);
+                    printTab(tasks);
+                    break;
+                default:
+                    System.out.println("Please select a correct option.");
+            }
+            printOptions(OPTIONS);
+        }
+    }
+}
